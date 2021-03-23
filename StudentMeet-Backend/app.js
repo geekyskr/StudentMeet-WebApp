32c9ba5402e24ff8d9d5cb2249f8b39a413ee47f
Sunil <sunilrk.one@gmail.com>
2021-03-23 15:48:37 +0530

    mysql connection established

--- StudentMeet-Backend/app.js
+++ StudentMeet-Backend/app.js
@@ -1,2 +1,17 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'sunil',
+  password: 'MysqL@3007',
+  database: 'StudentMeetDB'
+})
+connection.connect((err)=>{
+  if(err){
+    console.error("error occur while connecting "+err);
+    return;
+  }
+  console.log("connection established");
+});
