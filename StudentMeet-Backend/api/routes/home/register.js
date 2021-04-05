742580866b77272d3d63454942afd1ff8bbdb9d9
Sunil <sunilrk.one@gmail.com>
2021-04-05 22:34:30 +0530

    get request on register.js done

--- StudentMeet-Backend/api/routes/home/register.js
+++ StudentMeet-Backend/api/routes/home/register.js
@@ -2,8 +2,47 @@ const express = require('express');
 const router = express.Router();
 const mysqlConnection = require('../../../connection');
 
+
+
 router.get('/', (req, res, next)=>{
-  res.send("Register Page get");
+
+  const registerPageInformation = {}
+
+  function UniversityNameFunc(){
+    return new Promise((resolve, reject)=>{
+      const UniversityNameQuery = "select UniversityName from University";
+      mysqlConnection.query(UniversityNameQuery, (err, result, fields)=>{
+         if(!err)resolve(result)
+      })
+    })
+  }
+
+  function DegreeNameFunc(){
+    return new Promise((resolve, reject)=>{
+      const DegreeNameQuery = "select DegreeName from Degree";
+      mysqlConnection.query(DegreeNameQuery, (err, result, fields)=>{
+        if(!err)resolve(result)
+      })
+    })
+  }
+
+  function BranchNameFunc(){
+    return new Promise((resolve, reject)=>{
+      const BranchNameQuery = "select BranchName from Branch";
+      mysqlConnection.query(BranchNameQuery, (err, result, fields)=>{
+         if(!err)resolve(result)
+      })
+    })
+  }
+
+  async function main(){
+    registerPageInformation['UniversityNames'] = await UniversityNameFunc()
+    registerPageInformation['DegreeNames'] = await DegreeNameFunc()
+    registerPageInformation['BranchNames'] = await BranchNameFunc()
+    res.send(registerPageInformation);
+  }
+  main()
+
 })
 
 router.post('/', (req, res, next)=>{
