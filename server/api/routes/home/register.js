b1bd8da4964392836079c033d3debc7d74a93634
Sunil <sunilrk.one@gmail.com>
2021-04-17 15:44:30 +0530

    get data from register and store to database done

--- server/api/routes/home/register.js
+++ server/api/routes/home/register.js
@@ -12,7 +12,7 @@ router.get('/', (req, res, next)=>{
     return new Promise((resolve, reject)=>{
       const UniversityNameQuery = "select universityname from university";
       mysqlConnection.query(UniversityNameQuery, (err, result, fields)=>{
-         if(!err)resolve(result)
+        if(!err)resolve(result)
       })
     })
   }
@@ -25,11 +25,62 @@ router.get('/', (req, res, next)=>{
 
 })
 
+/*router.post('/', (req, res, next)=>{
+  const username = req.body.email;
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const college = req.body.college;
+  const number = req.body.number;
+  console.log(username+name+email+password+college+number)
+
+
+
+})*/
 
 // post request on home/register
 router.post('/', (req, res, next)=>{
-  res.send("post request on register.");
-})
+  const username = req.body.email;
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const college = req.body.college;
+  const number = req.body.number;
+  mysqlConnection.query("SELECT username FROM students WHERE username = '"+ username +"'", function(err, usresult, field){
+    console.log(usresult.length)
+    if(usresult.length > 0){
+      res.redirect('/login');
+    }
+    else {
+      //new user logic
+      console.log("we are inside")
+      mysqlConnection.query("SELECT universityid FROM university WHERE universityname = '"+ college +"'", function(err, unresult, field){
+        console.log(unresult.length)
+        if(unresult.lenght > 0){
+          console.log(unresult[0].universityid)
+          const dbquery = "insert into students values(?, ?, ?, ?, ?, ?);"
+          mysqlConnection.query(dbquery, [email, name, email, number, password, unresult[0].universityid], (err, result)=>{
+            if(err)console.log(err);
+            else res.send(result);
+          })
+        }
+      else {
+        //new university logic
+        console.log("we are deep level")
+        const cquery = "insert into university(universityname) values(?);"
+        mysqlConnection.query(cquery, [college], (err, result, field)=>{
+          console.log(field)
+          const dbquery = "insert into students values(?, ?, ?, ?, ?, ?);"
+          mysqlConnection.query(dbquery, [email, name, email, number, password, result[0].universityid], (err, result)=>{
+            if(err)console.log(err);
+            else res.send(result);
+          })
+          if(err)console.log(err);
+        })
+      }
 
-
+      })
+    }
+    })
+  })
 module.exports = router;
