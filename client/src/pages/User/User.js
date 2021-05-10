ca7fa5f56f1b38a31a99317b8bcffdf48cd16615
nippu <kumarnippu000006@gmail.com>
2021-05-10 19:56:32 +0530

    university page modify user interface modify login register button

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -11,15 +11,16 @@ function User(props){
             
             
             <div  class="sm-profile-center">
-                <h1 id="sm-profile-heading" class="sm-profile">Profile</h1>
+                <h1 id="sm-profile-heading" class="sm-profile"> Akhilesh swaroop gupta Profile</h1>
                 <div class="sm-profile-img-div">
                     <img class="sm-profile-img" src={userImg} alt="Profile Image" />
                 </div>
-            <h2 class="sm-profile">UserName</h2>
-            <h3 class="sm-profile">Email</h3>
+            <h2 class="sm-profile">UserName:kumarnippu000006@gmail.com</h2>
+            <h3 class="sm-profile">Email:kumarnippu000006@gmail.com</h3>
             <h3 class="sm-profile">University Name</h3>
             </div>
         </div>
+        {/* class="sm-profile-section2" contain edit and delete button */}
         <div class="sm-profile-section2">
             <div class="sm-profile-edit">
                 <button type="button" class="sm-p-edit">Edit Profile</button>
