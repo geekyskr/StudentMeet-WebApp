1b004bcd25924ded9b08f0887a04f0b1ed53a269
Sunil <sunilrk.one@gmail.com>
2021-05-23 16:39:24 +0530

    font-awesome and user link on post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -8,14 +8,14 @@ function Post(props){
       <div id="sm-single-post" class="card col-md-6" >
         <div class="card-body">
           <div class="sm-post-user">
-            <div><i class="fa fa-user-circle" aria-hidden="true"></i></div>
-            <div><h5>Sunil kumar Sharma</h5></div>
+            <div><a href={'/in/'+props.authour} class="fa fa-user-circle" aria-hidden="true"> Go to authour's profile
+            </a></div>
           </div>
 
           <h3 class="card-title" >{props.title}</h3>
           <h4 class="card-text" >{props.description}</h4>
           <p>{props.post}</p>
-          <h5>{props.tag}</h5>
+          <h6>{props.tag}</h6>
         <input id="sm-post-comment" class="sm-post-comment" type="text" placeholder="Comment..." />
         <div class="sm-post-icon">
           <div><i id="sm-post-like" class="fa fa-thumbs-up text-success bg-dark"></i></div>
@@ -24,7 +24,6 @@ function Post(props){
           <div><i id="sm-post-comment" class="fa fa-comment" aria-hidden="true"></i></div>
           <div><i id="sm-post-send" class="fa fa-paper-plane" aria-hidden="true"></i></div>
         </div>
-        <a href="#" class="btn btn-primary" >Read More</a>
       </div>
     </div>
   </div>
